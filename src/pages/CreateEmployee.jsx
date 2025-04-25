db18e6b2610372fb507c9cf9b3cce3827edc9ffc
Ma-Eum <cl.gachelin@gmail.com>
2025-04-25 14:00:19 +0200

    feat: ajout du composant Modal réutilisable

--- src/pages/CreateEmployee.jsx
+++ src/pages/CreateEmployee.jsx
@@ -1,10 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Modal from '../components/Modal/Modal'
 
 export default function CreateEmployee() {
+  const [showModal, setShowModal] = useState(false)
+
   return (
-    <section>
+    <>
       <h2>Créer un employé</h2>
-      {/* Formulaire à venir */}
-    </section>
+      <button onClick={() => setShowModal(true)}>Enregistrer</button>
+
+      <Modal
+        isOpen={showModal}
+        onClose={() => setShowModal(false)}
+        title="Employé créé"
+      >
+        <p>L'employé a bien été enregistré !</p>
+      </Modal>
+    </>
   )
 }

69f042a9e19323e3cc428585ea422e10be9a38ea
Ma-Eum <cl.gachelin@gmail.com>
2025-05-16 23:29:14 +0200

    Ajout d'une condition d'initialisation des employés fictifs dans employeesSlice

--- src/store/employeesSlice.js
+++ src/store/employeesSlice.js
@@ -1,7 +1,12 @@
+/* global process */
 import { createSlice } from '@reduxjs/toolkit'
+import { fakeEmployees } from '../data/fakeEmployees'
+
+// Génération automatique seulement en développement
+const shouldGenerateFakeEmployees = process.env.NODE_ENV !== 'production'
 
 const initialState = {
-  employees: [], // le store injectera le bon contenu via preloadedState
+  employees: shouldGenerateFakeEmployees ? fakeEmployees : [],
 }
 
 const employeesSlice = createSlice({
